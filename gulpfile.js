a570501a53346a322b8c18a09f7710c6ce94f2a5
nhadinat <nathanhadinata@gmail.com>
2015-09-08 13:12:31 -0700

    Gulp completed

--- gulpfile.js
+++ gulpfile.js
@@ -5,12 +5,30 @@ var gulp = require('gulp'),
 
 ///////////////* Stream *///////////////
 
+// Move to dist
+gulp.task('css', function() {
+  return gulp.src('css/*')
+  .pipe(gulp.dest('dist/css'));
+});
+
+// Move to dist
+gulp.task('js', ['css'], function() {
+  return gulp.src('js/*')
+  .pipe(gulp.dest('dist/js'));
+});
+
+// Move to dist
+gulp.task('html', ['js'], function() {
+  return gulp.src('./*.html')
+  .pipe(gulp.dest('dist/'));
+});
+
 // Publish to gh-pages
-gulp.task('deploy', ['inline'], function() {
-  return gulp.src('./dist/**/**/*')
+gulp.task('deploy', ['html'], function() {
+  return gulp.src('dist/**/*')
   .pipe(ghPages());
 });
 
 ///////////////* Default *///////////////
 // DEFAULT Group: Optimize, Build, then Deploy
-gulp.task('default', ['deploy']);
+gulp.task('default', ['css', 'js', 'html', 'deploy']);
