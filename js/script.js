ac994d8ae1f12f1f9a04689ac5395fc3c7bd301d
nhadinat <nathanhadinata@gmail.com>
2015-09-09 13:54:54 -0700

    Add NYT API

--- js/script.js
+++ js/script.js
@@ -16,13 +16,36 @@ function loadData() {
     var $city = $('#city').val();
     var loc = $street + ', ' + $city;
 
-    //Update greeting
+    // Update greeting
     $greeting.text("So, you want to live in " + loc + "?");
 
     var streetviewUrl = 'https://maps.googleapis.com/maps/api/' +
         'streetview?size=600x300&location=' + loc + '';
     $body.append('<img class="bgimg" src="' + streetviewUrl + '">');
 
+    // NYT Article Search Results
+    // AJAX Request
+    $.getJSON(
+        'http://api.nytimes.com/svc/search/v2/articlesearch.' +
+        'json?q=barack+obama' +
+        '&api-key=d063d63c7bf2373bfd5f718418f6e128:0:62722835',
+        function (data) {
+            console.log(data);
+
+            var article = data.response.docs;
+            var numArticles = article.length;
+
+            // Iterate response
+            for (var i = 0; i < numArticles; i++) {
+                $nytElem.text = article[i];
+                // <ul id="nytimes-articles" class="article-list"></ul>
+            }
+    });
+
+
+
+    // Update NYT Header
+    $nytHeaderElem.text("Recent New York Times Articles in " + loc);
 
     return false;
 }
