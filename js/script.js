095ed3653646dd2a119089144a63d856d9004041
nhadinat <nathanhadinata@gmail.com>
2015-09-09 16:15:02 -0700

    Fix getJSON request

--- js/script.js
+++ js/script.js
@@ -16,6 +16,7 @@ function loadData() {
   var $city = $('#city').val();
   var loc = $street + ', ' + $city;
 
+
   // Update greeting
   $greeting.text("So, you want to live in " + loc + "?");
 
@@ -24,46 +25,40 @@ function loadData() {
   $body.append('<img class="bgimg" src="' + streetviewUrl + '">');
 
   // NYT Article Search Results
-  // AJAX Request
-  $.getJSON(
-    'http://api.nytimes.com/svc/search/v2/articlesearch.' +
-    'json?q=' + loc +
-    '&api-key=d063d63c7bf2373bfd5f718418f6e128:0:62722835',
-    function (data) {
-      console.log(data);
-
-      /*
-        $.each( data.response.docs,
-          function( webUrl, headline, snippet ) {
-          items.push( "<li class='article'>" + "<a href='" + webUrl +
-          "'>" + headline + "</a><p>" + snippet + "</p></li>" );
-        });
-
-        $( "<ul/>", {
-          "class": "my-new-list",
-          html: items.join( "" )
-        }).appendTo( "body" );
-      */
-      var articles = data.response.docs;
-      var numArticles = articles.length;
-      var webUrl;
-      var snippet;
-      var headline;
-      // Iterate response
-      for (var i = 0; i < numArticles; i++) {
-        webUrl = articles[i].web_url;
-        snippet = articles[i].snippet;
-        headline = articles[i].headline.main;
-        item = "<li class='article'>" + "<a href='" + webUrl +
-          "'>" + headline + "</a><p>" + snippet + "</p></li>";
-        $nytElem.append(item);
-      }
-  });
-
-
 
   // Update NYT Header
-  $nytHeaderElem.text("Recent New York Times Articles in " + loc);
+  $nytHeaderElem.text("Recent New York Times Articles in " + $city);
+
+  var nytimesURL =
+    'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' +
+    $city + '&api-key=d063d63c7bf2373bfd5f718418f6e128:0:62722835';
+
+  // AJAX Request
+  $.getJSON(nytimesURL, function (data) {
+    console.log(data);
+
+    // Declare data object pieces
+    var articles = data.response.docs;
+    var numArticles = articles.length;
+    var webUrl;
+    var snippet;
+    var headline;
+
+    // Declare list item
+    var item;
+
+    // Iterate response
+    for (var i = 0; i < numArticles; i++) {
+      webUrl = articles[i].web_url;
+      snippet = articles[i].snippet;
+      // Prevent null result from showing
+      if (snippet === null){snippet = "Click to view article";}
+      headline = articles[i].headline.main;
+      item = "<li class='article'>" + "<a href='" + webUrl +
+        "'>" + headline + "</a><p>" + snippet + "</p></li>";
+      $nytElem.append(item);
+    }
+  });
 
   return false;
 }
